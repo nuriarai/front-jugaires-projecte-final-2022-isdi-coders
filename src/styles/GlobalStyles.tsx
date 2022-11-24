0318fcd9164ab1810978b37beedc5a61d55fad4c
Núria Ramoneda <nuriarai@gmail.com>
2022-11-24 14:41:11 +0100

    Rewrite the tabs for eslint issue

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -5,63 +5,62 @@ import "@fontsource/oranienbaum";
 const GlobalStyles = createGlobalStyle`
 
 *:where(:not(html, iframe, canvas, img, svg, video, audio, h1, h2, h3, h4, h5, p):not(svg *, symbol *)) {
-    all: unset;
-    display: revert;
+  all: unset;
+  display: revert;
 }
 
 /* Preferred box-sizing value */
 *,
 *::before,
 *::after {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 /* Reapply the pointer cursor for anchor tags */
 a, button {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 /* Remove list styles (bullets/numbers) */
 ol, ul, menu {
-    list-style: none;
-    margin: 0;
+  list-style: none;
+  margin: 0;
 }
 
 /* For images to not be able to exceed their container */
 img {
-    max-width: 100%;
+  max-width: 100%;
 }
 
 /* removes spacing between cells in tables */
 table {
-    border-collapse: collapse;
+  border-collapse: collapse;
 }
 
 /* Safari - solving issue when using user-select:none on the <body> text input doesn't working */
 input, textarea {
-    -webkit-user-select: auto;
+  -webkit-user-select: auto;
 }
 
 /* revert the 'white-space' property for textarea elements on Safari */
 textarea {
-    white-space: revert;
+  white-space: revert;
 }
 
 /* minimum style to allow to style meter element */
 meter {
-    -webkit-appearance: revert;
-    appearance: revert;
+  -webkit-appearance: revert;
+  appearance: revert;
 }
 
 /* reset default text opacity of input placeholder */
 ::placeholder {
-    color: unset;
+  color: unset;
 }
 
-/* fix the feature of 'hidden' attribute.
-   display:revert; revert to element instead of attribute */
+/* fix the feature of 'hidden' attribute.display:revert; revert to element instead of attribute */
 :where([hidden]) {
-    display: none;
+  display: none;
 }
 
 /* revert for bug in Chromium browsers
@@ -76,8 +75,8 @@ meter {
 }
 
 /* apply back the draggable feature - exist only in Chromium and Safari */
-:where([draggable="true"]) {
-    -webkit-user-drag: element;
+::after {
+  -webkit-user-drag: element;
 }
 
 button,
