e556ec6a02bb8c79d8bfdce6aafb608d29eace78
Núria Ramoneda <nuriarai@gmail.com>
2022-11-26 10:56:26 +0100

    Remove jest-styled-components import not used at least

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,4 +1,3 @@
-import "jest-styled-components";
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import mockUiState from "../../mooks/mockUiState";
