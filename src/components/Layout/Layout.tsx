b8122acfb23e262f7faab6b141bd2b2243806dcd
Núria Ramoneda <nuriarai@gmail.com>
2022-11-27 01:25:26 +0100

    Remove unused imports

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,7 @@
 import LayoutStyled from "./LayoutStyled";
-import Button from "../Button/Button";
-
-import { useAppDispatch } from "../../redux/hooks";
-import { showModalActionCreator } from "../../redux/features/uiSlice";
-import InputLabel from "../InputLabel/InputLabel";
 import RegisterForm from "../RegisterForm/RegisterForm";
 
 const Layout = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-
   return (
     <LayoutStyled>
       <main>
