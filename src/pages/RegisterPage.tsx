fc5a1cb96ee050063bc5a6ae63266102cb77060f
Núria Ramoneda <nuriarai@gmail.com>
2022-11-27 02:10:06 +0100

    Add Register Page

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+const RegisterPage = (): JSX.Element => {
+  return (
+    <>
+      <h1 className="page__title">Registre</h1>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default RegisterPage;
