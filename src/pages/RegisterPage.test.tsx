efb4032f291301931c91f8ae171753478cb45b54
Núria Ramoneda <nuriarai@gmail.com>
2022-11-28 12:01:31 +0100

    Add RegisterPage test

---
+++ src/pages/RegisterPage.test.tsx
@@ -0,0 +1,18 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../utils/testUtils/renderWithProviders";
+
+import RegisterPage from "./RegisterPage";
+
+describe("Given a Register page", () => {
+  describe("When its rendered", () => {
+    test("Then it should show a heading level 1 with the text 'Registre'", () => {
+      const headingText = "Registre";
+
+      renderWithProviders(<RegisterPage />);
+
+      const heading = screen.queryByRole("heading", { level: 1 });
+
+      expect(heading).toHaveTextContent(headingText);
+    });
+  });
+});
