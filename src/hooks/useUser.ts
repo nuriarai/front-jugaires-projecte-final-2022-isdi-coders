cf385e6058cf0dbdd0412da2b7529ac0c6961afa
Núria Ramoneda <nuriarai@gmail.com>
2022-11-28 09:50:35 +0100

    Add loginUser custom hook

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,14 +1,17 @@
 import axios, { AxiosError } from "axios";
 import { showModalActionCreator } from "../redux/features/uiSlice";
+import { loginUserActionCreator } from "../redux/features/userSlice/userSlice";
 import { useAppDispatch } from "../redux/hooks";
 
-import { UserRegisterData } from "../types/types";
-import { AxiosResponse } from "./types";
+import { UserCredentialsData, UserRegisterData } from "../types/types";
+import decodeToken from "../utils/decodeToken";
+import { AxiosResponse, JwtCustomPayload } from "./types";
 
 const apiUrl = process.env.REACT_APP_API;
 
 const useUser = () => {
   const dispatch = useAppDispatch();
+
   const userRegister = async (registerData: UserRegisterData) => {
     try {
       await axios.post(`${apiUrl}/users/register`, registerData);
@@ -27,9 +30,36 @@ const useUser = () => {
       );
     }
   };
+
+  const userLogin = async (loginData: UserCredentialsData) => {
+    try {
+      const response = await axios.post(`${apiUrl}/users/login`, loginData);
+
+      const { token } = await response.data;
+
+      const userLogged: JwtCustomPayload = decodeToken(token);
+
+      dispatch(
+        loginUserActionCreator({
+          ...userLogged,
+          token,
+        })
+      );
+
+      localStorage.setItem("token", token);
+    } catch (error: unknown) {
+      dispatch(
+        showModalActionCreator({
+          modalType: "error",
+          message: (error as AxiosError<AxiosResponse>).response?.data.error!,
+        })
+      );
+    }
+  };
+
   return {
     userRegister,
+    userLogin,
   };
 };
-
 export default useUser;
