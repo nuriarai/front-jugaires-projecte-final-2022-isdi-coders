fb3e4935bdd4bba4ea1909dc48b6120676fe33f8
Núria Ramoneda <nuriarai@gmail.com>
2022-11-28 18:13:33 +0100

    Add layout and app components

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LayoutStyled from "./LayoutStyled";
 import RegisterPage from "../../pages/RegisterPage/RegisterPage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 const Layout = (): JSX.Element => {
   return (
@@ -11,7 +12,7 @@ const Layout = (): JSX.Element => {
           <Route path="/" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<p>Camí no resolt</p>} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
     </LayoutStyled>
