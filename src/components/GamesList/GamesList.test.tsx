da8eeb230f82ce1a9fc22f34f23cd52bf1240495
Núria Ramoneda <nuriarai@gmail.com>
2022-12-01 19:48:34 +0100

    Add GamesList component & styles & test

---
+++ src/components/GamesList/GamesList.test.tsx
@@ -0,0 +1,34 @@
+import { screen } from "@testing-library/react";
+import { mockGamesList } from "../../mooks/mocksGames";
+import mockUiState from "../../mooks/mockUiState";
+import mockUserState from "../../mooks/mockUserState";
+import renderWithProviders from "../../utils/testUtils/renderWithProviders";
+import GamesList from "./GamesList";
+
+const mockLoadGames = jest.fn();
+
+jest.mock("../../hooks/useGames/useGames", () => {
+  return () => ({
+    loadGames: mockLoadGames,
+  });
+});
+
+describe("Given a list of games component", () => {
+  describe("When it's rendered with a lsit of 2 games", () => {
+    test("Then it should show a list of the games returned", () => {
+      const numExpectedGames = 2;
+
+      renderWithProviders(<GamesList />, {
+        preloadedState: {
+          ui: mockUiState,
+          user: mockUserState,
+          game: { list: mockGamesList },
+        },
+      });
+
+      const gamesList = screen.queryAllByRole("listitem");
+
+      expect(gamesList).toHaveLength(numExpectedGames);
+    });
+  });
+});
