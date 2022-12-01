395586c81b1be35c3965d9ba32c5cdd819a53dcc
Núria Ramoneda <nuriarai@gmail.com>
2022-12-01 15:01:12 +0100

    Add store with game reducer and mockinitialStore

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { gamesReducer } from "./features/gamesSlice/gamesSlice";
 import { uiReducer } from "./features/uiSlice";
 import { userReducer } from "./features/userSlice/userSlice";
 
@@ -6,6 +7,7 @@ export const store = configureStore({
   reducer: {
     ui: uiReducer,
     user: userReducer,
+    game: gamesReducer,
   },
 });
 
