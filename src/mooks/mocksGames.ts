fcf7760af38146a908b819947eb17024ba0a8319
Núria Ramoneda <nuriarai@gmail.com>
2022-12-14 14:30:53 +0100

    Add mockGameEmpty to games mocksfor detail

--- src/mooks/mocksGames.ts
+++ src/mooks/mocksGames.ts
@@ -1,14 +1,33 @@
 import { getRandomGames } from "../factories/gameFactory";
-import { GamesState } from "../types/gameTypes";
+import { Game, GamesState } from "../types/gameTypes";
 
 export const mockGamesListOfFive = getRandomGames(5);
 
 export const mockGamesList = getRandomGames(2);
 
+export const mockOneGame = getRandomGames(1);
+
+export const mockGameEmpty: Game = {
+  id: "",
+  owner: "",
+  gameBoard: "",
+  dateTime: "",
+  picture: "",
+  pictureBackup: "",
+  location: "",
+  addressLocation: "",
+  minPlayers: 2,
+  maxPlayers: 2,
+  duration: "",
+  observations: "",
+};
+
 export const mockInitialGameState: GamesState = {
   list: [],
+  currentGame: {} as Game,
 };
 
 export const gamesListOfFive: GamesState = {
   list: mockGamesListOfFive,
+  currentGame: mockOneGame[0],
 };
