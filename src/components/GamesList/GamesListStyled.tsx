b8b24574568380bf2f97d90f207a1f98ff21164d
Núria Ramoneda <nuriarai@gmail.com>
2022-12-17 21:52:57 +0100

    Remove functionality from component & css changes

--- src/components/GamesList/GamesListStyled.tsx
+++ src/components/GamesList/GamesListStyled.tsx
@@ -4,6 +4,19 @@ const GamesListStyled = styled.ul`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+
+  display: grid;
+  grid-template-columns: 1, 335px;
+  grid-auto-rows: 1fr;
+  row-gap: 30px;
+  justify-content: center;
+
+  @media (min-width: ${(props) => props.theme.mediaSizes.medium}) {
+    grid-template-columns: repeat(2, 335px);
+    justify-content: space-between;
+  }
+  @media (min-width: ${(props) => props.theme.mediaSizes.big}) {
+    grid-template-columns: repeat(3, 335px);
+  }
 `;
 export default GamesListStyled;
