2e514820606dfd186dfc6d81570a9d0795f15fc8
Núria Ramoneda <nuriarai@gmail.com>
2022-12-18 20:02:07 +0100

    Add cases testing new pagination and filter components

--- src/pages/GamesPage/GamesPage.test.tsx
+++ src/pages/GamesPage/GamesPage.test.tsx
@@ -1,9 +1,33 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { mockInitialGameState } from "../../mooks/mocksGames";
+import mockUiState from "../../mooks/mockUiState";
+import mockUserState from "../../mooks/mockUserState";
+import {
+  filterGamesActionCreator,
+  nextGamesPageActionCreator,
+} from "../../redux/features/gamesSlice/gamesSlice";
+import { store } from "../../redux/store";
 import renderWithProviders from "../../utils/testUtils/renderWithProviders";
 import GamesPage from "./GamesPage";
 
+const mockLoadGames = jest.fn();
+
+jest.mock("../../hooks/useGames/useGames", () => {
+  return () => ({
+    loadGames: mockLoadGames,
+  });
+});
+
+const mockDispatch = jest.fn();
+
+jest.mock("../../redux/hooks", () => ({
+  ...jest.requireActual("../../redux/hooks"),
+  useAppDispatch: () => mockDispatch,
+}));
+
 describe("Given a Games page", () => {
-  describe("When its rendered", () => {
+  describe("When is rendered", () => {
     test("Then it should show a heading level 1 with the text 'Partides'", () => {
       const headingText = "Partides";
 
@@ -12,6 +36,92 @@ describe("Given a Games page", () => {
       const heading = screen.queryByRole("heading", { level: 1 });
 
       expect(heading).toHaveTextContent(headingText);
+    });
+
+    test("Then it should call the loadGames action", async () => {
+      renderWithProviders(<GamesPage />, {
+        store: store,
+      });
+
+      expect(mockLoadGames).toHaveBeenCalled();
+    });
+  });
+
+  describe("When is rendered with the GameBoard select in default", () => {
+    test("Then it should call the loadGames action", async () => {
+      renderWithProviders(<GamesPage />, {
+        store: store,
+      });
+
+      const filterSelect = screen.getByRole("combobox", {
+        name: "Filtra per joc:",
+      });
+
+      await userEvent.selectOptions(
+        filterSelect,
+        screen.getByRole("option", { name: "Totes les partides" })
+      );
+
+      expect(mockLoadGames).toHaveBeenCalled();
+    });
+  });
+
+  describe("When is rendered and it's button 'Més partides' is clicked", () => {
+    test("Then it should dispatch nextGamesPageActionCreator", async () => {
+      renderWithProviders(<GamesPage />, {
+        preloadedState: {
+          ui: mockUiState,
+          user: mockUserState,
+          game: {
+            ...mockInitialGameState,
+            pages: {
+              currentPage: 0,
+              isNextPage: true,
+              totalPages: 2,
+            },
+          },
+        },
+      });
+
+      const loadMoreButton = screen.getByRole("button", {
+        name: "Carregar més partides",
+      });
+
+      await userEvent.click(loadMoreButton);
+
+      expect(mockDispatch).toHaveBeenCalledWith(nextGamesPageActionCreator());
+    });
+  });
+
+  describe("When it's select gameBoard is selected", () => {
+    test("Then it should call filterGamesActionCreator withe gameBoard", async () => {
+      renderWithProviders(<GamesPage />, {
+        preloadedState: {
+          ui: mockUiState,
+          user: mockUserState,
+          game: {
+            ...mockInitialGameState,
+            pages: {
+              currentPage: 0,
+              isNextPage: true,
+              totalPages: 2,
+            },
+          },
+        },
+      });
+
+      const filterSelect = screen.getByRole("combobox", {
+        name: "Filtra per joc:",
+      });
+
+      await userEvent.selectOptions(
+        filterSelect,
+        screen.getByRole("option", { name: "Butifarra" })
+      );
+
+      expect(mockDispatch).toHaveBeenCalledWith(
+        filterGamesActionCreator({ gameBoard: "Butifarra" })
+      );
     });
   });
 });
