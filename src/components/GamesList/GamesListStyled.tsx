725ddb29116d2b4f51eb28ffc55a8183d09f889f
Núria Ramoneda <nuriarai@gmail.com>
2022-12-19 03:08:06 +0100

    Remove background for filter

--- src/components/GamesList/GamesListStyled.tsx
+++ src/components/GamesList/GamesListStyled.tsx
@@ -2,21 +2,22 @@ import styled from "styled-components";
 
 const GamesListStyled = styled.ul`
   width: 100%;
+  margin-bottom: 30px;
   display: flex;
   flex-wrap: wrap;
 
   display: grid;
   grid-template-columns: 1, 335px;
-  grid-auto-rows: 1fr;
   row-gap: 30px;
   justify-content: center;
 
   @media (min-width: ${(props) => props.theme.mediaSizes.medium}) {
-    grid-template-columns: repeat(2, 335px);
+    grid-template-columns: repeat(auto-fill, 335px);
+    column-gap: 15px;
+    grid-auto-rows: 1fr;
     justify-content: space-between;
   }
   @media (min-width: ${(props) => props.theme.mediaSizes.big}) {
-    grid-template-columns: repeat(3, 335px);
   }
 `;
 export default GamesListStyled;
